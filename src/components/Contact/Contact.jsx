43ccc35f643ceff903171d09d299db16c75eee76
Vitalii Tymkiv <vitaliy.tymkiv@gmail.com>
2022-12-04 21:14:49 +0200

    add delete icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { ContactItem, Name, Btn } from './Contact.styled';
 import PropTypes from 'prop-types';
+import { RiDeleteBin6Fill } from 'react-icons/ri';
 
 export const Contact = ({ name, phone, onDelete }) => {
   return (
@@ -8,7 +9,7 @@ export const Contact = ({ name, phone, onDelete }) => {
         {name}: {phone}
       </Name>
       <Btn type="button" onClick={onDelete}>
-        Delete
+        Delete <RiDeleteBin6Fill></RiDeleteBin6Fill>
       </Btn>
     </ContactItem>
   );
